c8208a485a9319e0d37d096db2d2eb3d76b5a048
Kevin Ly <kly1@avans.nl>
2018-05-24 18:06:55 +0200

    fix incoming ship event model and send

--- DockService.App/Messaging/DockEventHandler.cs
+++ DockService.App/Messaging/DockEventHandler.cs
@@ -9,62 +9,62 @@ using Utf8Json;
 
 namespace DockService.App.Messaging
 {
-    public class DockEventHandler : IEventHandlerCallback
-    {
-        private readonly IDockService _dockService;
-        public DockEventHandler(IDockService dockService)
-        {
-            _dockService = dockService;
-        }
+	public class DockEventHandler : IEventHandlerCallback
+	{
+		private readonly IDockService _dockService;
+		public DockEventHandler(IDockService dockService)
+		{
+			_dockService = dockService;
+		}
 
-        #region --- main stuff---
-        public async Task<bool> HandleEventAsync(EventTypes eventType, string message)
-        {
-            switch (eventType)
-            {
-                case EventTypes.ShipNearingHarbor:
-                    {
-                        return await HandleShipNearingHarbor(message);
-                    }
-                case EventTypes.ShipUndock:
-                    {
-                        return await HandleShipUndock(message);
-                    }
-                case EventTypes.Unknown:
-                    {
-                        return true;
-                    }
-            }
+		#region --- main stuff---
+		public async Task<bool> HandleEventAsync(EventTypes eventType, string message)
+		{
+			switch (eventType)
+			{
+				case EventTypes.ShipNearingHarbor:
+					{
+						return await HandleShipNearingHarbor(message);
+					}
+				case EventTypes.ShipUndock:
+					{
+						return await HandleShipUndock(message);
+					}
+				case EventTypes.Unknown:
+					{
+						return true;
+					}
+			}
 
-            return true;
-        }
-        #endregion
+			return true;
+		}
+		#endregion
 
-        #region --- Incoming Dock and Undock events ---
-        private async Task<bool> HandleShipNearingHarbor(string message)
-        {
+		#region --- Incoming Dock and Undock events ---
+		private async Task<bool> HandleShipNearingHarbor(string message)
+		{
 
-            Ship receivedShip = JsonSerializer.Deserialize<Ship>(message);
-            Ship createdShip = await _dockService.CreateShipAsync(receivedShip);
-            //Send tugboats to assist with undocking
-            await _dockService.SendTugboatDispatchedAsync(createdShip);
-            //Execute docking method
-            await _dockService.SendShipDockedAsync(createdShip);
+			var receivedShip = JsonSerializer.Deserialize<DockEventModel>(message);
+			Ship createdShip = await _dockService.CreateShipAsync(new Ship() { Id = receivedShip.ShipId, Name = receivedShip.ShipName, Containers = receivedShip.Containers as List<Container>, CustomerId = receivedShip.CustomerId });
+			//Send tugboats to assist with undocking
+			await _dockService.SendTugboatDispatchedAsync(createdShip);
+			//Execute docking method
+			await _dockService.SendShipDockedAsync(createdShip);
 
-            return true;
-        }
+			return true;
+		}
 
-        private async Task<bool> HandleShipUndock(string message)
-        {
+		private async Task<bool> HandleShipUndock(string message)
+		{
 
-            Ship receivedShip = JsonSerializer.Deserialize<Ship>(message);
-            //Send tugboats to assist with undocking
-            await _dockService.SendTugboatDispatchedAsync(receivedShip);
-            //Execute undocking method
-            await _dockService.SendShipUndockedAsync(receivedShip);
-            return true;
-        }
-        #endregion
+			var receivedShip = JsonSerializer.Deserialize<DockEventModel>(message);
+			//Send tugboats to assist with undocking
+			await _dockService.SendTugboatDispatchedAsync(new Ship() { Id = receivedShip.ShipId, Name = receivedShip.ShipName, Containers = receivedShip.Containers as List<Container>, CustomerId = receivedShip.CustomerId });
+			//Execute undocking method
+			await _dockService.SendShipUndockedAsync(new Ship() { Id = receivedShip.ShipId, Name = receivedShip.ShipName, Containers = receivedShip.Containers as List<Container>, CustomerId = receivedShip.CustomerId });
+			return true;
+		}
+		#endregion
 
-    }
+	}
 }
